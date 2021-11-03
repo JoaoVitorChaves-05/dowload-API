5a992df75c52c23c944d7e3d81245a9e8261beae
João Vitor Mancio Chaves <joaovmanciochaves@gmail.com>
2021-11-03 17:18:52 -0300

    heroku commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+
 const ytdl = require('ytdl-core');
 
+const PORT = process.env.PORT || 3000
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', async function(req, res) {
@@ -17,4 +19,4 @@ app.get('/', async function(req, res) {
     
 });
 
-app.listen(3000)
+app.listen(PORT)
